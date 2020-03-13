61d060e20700e4855d1700572b664646c59338fb
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-13 10:53:43 +0000

    Unsure if finished.

--- actionRoutes.js
+++ actionRoutes.js
@@ -14,7 +14,49 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    actionHelpers.get(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    });
+});
 
+router.post('/:id/addAction', (req, res) => {
+    const action = req.body;
+    actionHelpers.insert(action)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    actionHelpers.delete(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    actionHelpers.update(id, req.body)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const helmet = require('helmet')
 const server = express();
 
 const projectsRoutes = require('./projectsRoutes')
+const actionRoutes = require('./actionRoutes')
 
 server.use(express.json())
 server.use(helmet())
 server.use('/projectsRoutes', projectsRoutes)
-
+server.use('/actionRoutes', actionRoutes)
 server.get('/', (req, res) => {
     res.send(`<h2>Server Runs...</h2>`)
 });
