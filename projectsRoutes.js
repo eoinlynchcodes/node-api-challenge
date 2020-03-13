ee8671a9ec22b8e133d14f709b9bc51b7e4c7db4
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-13 09:39:48 +0000

    get projects by id is working.

--- projectsRoutes.js
+++ projectsRoutes.js
@@ -14,5 +14,19 @@ projectsHelpers.get()
 });
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    projectsHelpers.get(id)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json(error)
+    });
+});
+
+
+
 
 module.exports = router;
